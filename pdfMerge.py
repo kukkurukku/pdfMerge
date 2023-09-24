0d3636a6ed666c118d31fde5b3acd438e3633dd1
kukkurukku <sajih.binsuja5@gmail.com>
2023-09-25 02:04:14 +0600

    Added Drag and Drop

--- pdfMerge.py
+++ pdfMerge.py
@@ -1,6 +1,6 @@
 import sys, os, io
 import typing
-from PyQt5 import QtCore
+from PyQt5 import QtCore, QtGui
 if hasattr(sys, 'frozen'): #making_a_standAlone_installer
     os.environ['PATH'] = sys._MEIPASS + ';' + os.environ['PATH'] 
 
@@ -29,11 +29,48 @@ def resource_path(relative_path):
 
 #creatingClasses
 
+
+#dragAndDropFeature
+class ListWidget(QListWidget):
+    def __init__(self, parent: QWidget | None = ...) -> None:
+        super().__init__(parent)
+        self.setAcceptDrops(True)
+        self.setStyleSheet('font-size: 25px;')
+        self.setDragDropMode(QAbstractItemView.InternalMove)
+        self.setSelectionMode(QAbstractItemView.ExtendedSelection)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            return super().dragEnterEvent()
+    
+    def dragMoveEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.setDropAction(Qt.CopyAction)
+            event.accept()
+        else:
+            return super().dragMoveEvent(event)
+    
+    def dropEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.setDropAction(Qt.CopyAction)
+            event.accept()
+
+            pdfFiles = []
+
+            for url in evnet.mimeData().urls():
+                if url.isLocalFile():
+                    if url.toString().endswith('.pdf'):
+                                               pdfFiles.append(str(url.toLocalFile()))
+        else:    
+            return super().dropEvent(event)
+
 class PDFApp(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('PDF Bundler')  #appTitle
-        self.setWindowIcon(QIcon(resource_path('DesignAIconInIllustrator')))  #AppIcon #need to add an icon
+        #self.setWindowIcon(QIcon(resource_path('DesignAIconInIllustrator')))  #AppIcon #need to add an icon
         self.resize(960, 540) #windowSize
 
 app = QApplication(sys.argv)
